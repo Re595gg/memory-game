9dc04d2f0d8f41d053de8226141978732977fa3c
Renato Galindo (re595gg) <renato_galindo@outlook.com>
2026-06-27 00:29:23 -0400

    update: 2026-06-26

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -4,27 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // @ts-ignore
-import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink  } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';  
-import 'bootstrap-icons/bootstrap-icons.svg' 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/bootstrap-icons.svg'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { BrowserRouter } from 'react-router-dom';
 
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
-const HOSTNAME = process.env.HOSTNAME || "https://memory-game-test.onrender.com"; 
+const HOSTNAME = process.env.HOSTNAME || "https://memory-game-test.onrender.com";
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
 
 const httpLink = new HttpLink({
-  uri: `https://memory-game-test.onrender.com/api/gql/`, 
+  uri: `https://memory-game-test.onrender.com/api/gql/`,
 })
 const wsLink = new GraphQLWsLink(createClient({
   url: `wss://memory-game-test.onrender.com/api/gql`,
-})); 
+}));
 
 const splitLink = split(
   ({ query }) => {
@@ -36,19 +36,19 @@ const splitLink = split(
   },
   wsLink,
   httpLink,
-); 
+);
 
 const client = new ApolloClient({
-  link: splitLink, 
+  link: splitLink,
   cache: new InMemoryCache(),
 
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <App/>
-      </ApolloProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
 reportWebVitals();
